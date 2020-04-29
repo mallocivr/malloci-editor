80e90626e23f34da663bd9e8e090f814dc92144b
Michael Gutensohn <michael@gutensohn.me>
2020-04-29 01:52:22 -0700

    Updates

---
+++ precache-manifest.6e8d6b2418cdad786d6b5ccad288cfdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc13b93bbc9c1e90b8240e0be5a2914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd19bdaea010a50797e",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "b555ea50285cd059f29f",
+    "url": "/static/css/main.b63a6b06.chunk.css"
+  },
+  {
+    "revision": "9fd19bdaea010a50797e",
+    "url": "/static/js/2.28f5bc8d.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.28f5bc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b555ea50285cd059f29f",
+    "url": "/static/js/main.9b01cb29.chunk.js"
+  },
+  {
+    "revision": "d7250f48c1e4ac4df93f",
+    "url": "/static/js/runtime-main.7fee2178.js"
+  }
+]);
